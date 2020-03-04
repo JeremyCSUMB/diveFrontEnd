c2692e1773ab99512ed0ebf34c1b97a83091897b
Elijah Baird <elijah_baird84@yahoo.com>
2020-03-03 19:57:41 -0800

    Added example chart

---
+++ src/components/Chart.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="small">
+    <line-chart :chart-data="datacollection"></line-chart>
+    <button @click="fillData()">Randomize</button>
+  </div>
+</template>
+
+<script>
+import LineChart from '../constants/LineChart.js'
+
+export default {
+  components: {
+    LineChart
+  },
+  data () {
+    return {
+      datacollection: null
+    }
+  },
+  mounted () {
+    this.fillData()
+  },
+  methods: {
+    fillData () {
+      this.datacollection = {
+        labels: [this.getRandomInt(), this.getRandomInt()],
+        datasets: [
+          {
+            label: 'Data',
+            fill: 'false',
+            backgroundColor: 'cornflowerblue',
+            borderColor: 'cornflowerblue',
+            data: [this.getRandomInt(), this.getRandomInt(), this.getRandomInt(), this.getRandomInt()]
+          }
+        ]
+      }
+    },
+    getRandomInt () {
+      return Math.floor(Math.random() * (50 - 5 + 1)) + 5
+    }
+  }
+}
+</script>
+
+<style>
+  .small {
+    max-width: 600px;
+    margin:  150px auto;
+  }
+</style>
