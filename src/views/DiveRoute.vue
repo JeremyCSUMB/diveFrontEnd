7bf04efb93ce4e726e2c0ad5b3de075f89d9f022
Elijah Baird <elijah_baird84@yahoo.com>
2020-04-23 14:06:21 -0700

    Adding styling to DiveRoute

--- src/views/DiveRoute.vue
+++ src/views/DiveRoute.vue
@@ -1,8 +1,8 @@
 <template>
-  <div id="container">
-    <div id="main">
-      <div id="topSection">
-        <div id='diveInfo' v-if = "diveInfo !== null" class="topSectionElement">
+  <div id='main'>
+    <div id='topSection' class='grid-container-top'>
+      <div class='grid-item'>
+        <div id='diveInfo' v-if = 'diveInfo !== null' class='topSectionElement'>
           <h5>Dive Information</h5>
           <b>Chief Scientist:</b> {{diveInfo.chiefScientist}}<br><br>
           <b>Accomplishments:</b> {{diveInfo.briefAccomplishments}}<br><br>
@@ -13,30 +13,49 @@
           <b>ROV Name:</b> {{diveInfo.rovName}}<br><br>
           <b>Dive Number:</b> {{diveInfo.diveNumber}}<br><br>
         </div>
-        <DataError class="topSectionElement"/>
       </div>
-      <div id="downloadbuttons">
-        <toggle-button ref="downloadtoggle"
-          @change="onToggleUpdate()"
-          :value="togglestatus"
-          :labels="{checked: 'CSV', unchecked: 'JSON'}"
-          :width="60"
-          :color="{unchecked: 'blue'}"/><br>
-        <button v-on:click="jsonOrCsv('ctd')" id='ctdbutton' disabled>Download CTD</button>
-        <button v-on:click="jsonOrCsv('navigation')" id='navbutton' disabled>Download Navigation</button>
+      <div class='grid-item'>
+        <DataError class='topSectionElement'/>
       </div>
-      <div id="charts">
+    </div>
+
+    <br>
+
+    <div id='downloadbuttons'>
+      <toggle-button ref='downloadtoggle'
+        @change='onToggleUpdate()'
+        :value='togglestatus'
+        :labels='{checked: "CSV", unchecked: "JSON"}'
+        :width='60'
+        :color='{unchecked: "#2196F3", checked: "#4CAF50"}'/><br>
+      <button class='button' v-on:click='jsonOrCsv("ctd")' id='ctdbutton' disabled>Download CTD</button>
+      <button class='button' v-on:click='jsonOrCsv("navigation")' id='navbutton' disabled>Download Navigation</button>
+    </div>
+    <br>
+    <br>
+    <div class='grid-container-bottom'>
+      <div class='grid-item'>
         <TravelMap class='map'/>
+      </div>
+      <div class='grid-item'>
         <Chart class='chart'/>
       </div>
-      <AncillaryChart class='aChart'/>
-      <OTSChart class='ots'/>
-      <div>
-        <button v-on:click="directToPhotoPage()">View Photos</button>
-        <button v-on:click="directToVideoPage()">View video</button>
+      <div class='grid-item'>
+        <AncillaryChart class='aChart'/>
+      </div>
+      <div class='grid-item'>
+        <OTSChart class='ots'/>
       </div>
       <br>
     </div>
+
+    <br>
+    <div>
+      <button class='button' v-on:click='directToPhotoPage()'>View Photos</button>
+      <button class='button' v-on:click='directToVideoPage()'>View Video</button>
+    </div>
+
+    <br>
   </div>
 </template>
 
@@ -93,6 +112,13 @@ export default {
 
     onToggleUpdate () {
       this.togglestatus = !this.togglestatus
+      if (this.togglestatus === false) {
+        document.getElementById('ctdbutton').style.backgroundColor = '#2196F3'
+        document.getElementById('navbutton').style.backgroundColor = '#2196F3'
+      } else {
+        document.getElementById('ctdbutton').style.backgroundColor = '#4CAF50'
+        document.getElementById('navbutton').style.backgroundColor = '#4CAF50'
+      }
     },
 
     jsonOrCsv (datatype) {
@@ -191,7 +217,7 @@ export default {
 }
 
 #topSection:after {
-  content: "";
+  content: '';
   display: table;
   clear: both;
 }
@@ -203,37 +229,79 @@ export default {
 }
 
 #charts:after {
-  content: "";
+  content: '';
   display: table;
   clear: both;
 }
 
 .map {
-  float: left;
-  width: 50%;
-  height: 500px;
+  width: 80%;
+  height: 80%;
+  margin: auto;
 }
 
 .chart {
-  float: right;
-  width: 50%;
+  padding-top: 10%;
+  margin: auto;
 }
 
 .ots {
-  // float: right;
-  width: 400px;
   position: relative;
-  right: 20px
+  margin: auto;
 }
 
 .aChart {
-  width: 500px;
+  padding-top: 5%;
+  height: 600px;
+  margin: auto;
 }
 
 #main {
   margin: 0 auto;
   padding-left: 100px;
+  padding-right: 100px;
   padding-bottom: 50px;
+}
+
+.grid-container-top {
+  display: grid;
+  grid-template-columns: 50% 50%;
+  background-color: #2196F3;
+  padding: 10px;
+}
+
+.grid-container-bottom {
+  display: grid;
+  grid-template-columns: 50% 50%;
+  grid-template-rows: 50% 50%;
+  background-color: #2196F3;
+  padding: 10px;
+}
+
+.grid-item {
+  background-color: rgba(255, 255, 255, 0.8);
+  border: 1px solid rgba(0, 0, 0, 0.8);
+  text-align: center;
+  font-size: 10pt;
+}
+
+.button {
+  background-color: #2196F3; /* Green */
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  cursor: pointer;
+  -webkit-transition-duration: 0.4s; /* Safari */
+  transition-duration: 0.4s;
+}
+
+.button:hover {
+  box-shadow: 0 12px 16px 0 rgba(0,0,0,0.24),0 17px 50px 0 rgba(0,0,0,0.19);
 }
 
 </style>
